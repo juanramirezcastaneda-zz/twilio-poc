cf7b835fe46e7ef1ad575f9f72ae1102a4e4b932
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-03-11 17:13:26 +0100

    remove not needed routes

--- index.js
+++ index.js
@@ -19,18 +19,6 @@ var randomName = require('./randomname');
 // Create Express webapp.
 var app = express();
 
-// Set up the paths for the examples.
-[
-  'bandwidthconstraints',
-  'codecpreferences',
-  'localvideofilter',
-  'localvideosnapshot',
-  'mediadevices',
-].forEach(function(example) {
-  var examplePath = path.join(__dirname, `../examples/${example}/public`);
-  app.use(`/${example}`, express.static(examplePath));
-});
-
 // Set up the path for the quickstart.
 var quickstartPath = path.join(__dirname, '../quickstart/public');
 app.use('/quickstart', express.static(quickstartPath));
@@ -42,8 +30,8 @@ app.use('/examples', express.static(examplesPath));
 /**
  * Default to the Quick Start application.
  */
-app.get('/', function(request, response) {
-  response.redirect('/quickstart');
+app.get('/', function(_request, response) {
+  response.sendFile(process.cwd() + '/public/index.html');
 });
 
 /**

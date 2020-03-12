ed9bb95e571d8360ea29aa83f2a0bbb3619fe15e
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-03-12 17:31:30 +0100

    set mode by default to development

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './public/index.js',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'public'),

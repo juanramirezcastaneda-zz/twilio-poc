10466bb3b7701af82446f12b2de7284c378a8195
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-03-17 17:09:24 +0100

    add new endpoint to get recordings information.

--- server.js
+++ server.js
@@ -27,18 +27,26 @@ app.get('/', function(_request, response) {
 
 app.get('/twilio', function(request, response) {
   const accountSid = 'AC5228aa17a5bc82b642f97194bdaf7aa7';
-  const authToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzA0MjBhOTU5YzdkODczNDU3ZTMxZDk5NTZiYzk4OTI5LTE1ODQxMTcxNDciLCJncmFudHMiOnsiaWRlbnRpdHkiOiJKb2huIERvZSIsInZpZGVvIjp7InJvb20iOiJUZXN0Um9vbSJ9fSwiaWF0IjoxNTg0MTE3MTQ3LCJleHAiOjE1ODQxMjA3NDcsImlzcyI6IlNLMDQyMGE5NTljN2Q4NzM0NTdlMzFkOTk1NmJjOTg5MjkiLCJzdWIiOiJBQzUyMjhhYTE3YTViYzgyYjY0MmY5NzE5NGJkYWY3YWE3In0.MopNtoC6neguvwWCwJMAmw-zqs3KudeFV9BFFgljLPI';
+  const authToken = request.query.token;
   const client = require('twilio')(accountSid, authToken);
 
-  client.video.recordings
-    .list({
-      groupingSid: ['RM4bad6dc630af894252d7c97fe45f2caa'],
-      limit: 20,
-    })
-    .then(recordings => {
-      recordings.forEach(r => console.log(r.sid));
-      response.json({ recordings });
+  console.log(client);
+
+  response.send('Fuck off');
+});
+
+app.get('/twilio-video', function(request, response) {
+  const accountSid = 'AC5228aa17a5bc82b642f97194bdaf7aa7';
+  const authToken = request.query.token;
+  const client = require('twilio')(accountSid, authToken);
+
+  const recordId = request.query.sid;
+  client.video
+    .recordings(recordId)
+    .fetch()
+    .then(recording => {
+      console.info(recording);
+      response.json(recording);
     });
 });
 
